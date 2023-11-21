e66152b148d48a09e45b5c7e7e2145206f608570
zhizi6 <139207819+zhiziqixingdeng@users.noreply.github.com>
2023-11-21 19:03:52 +0800

    Add files via upload

---
+++ 客户/index.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string name;
+int ans;
+struct blog{
+	string mz;//名字
+	string zt;//主题
+	int flag;//是否删除
+	string nr;//内容
+}s[9999999];
+string ss="./index.exe";
+int main(){
+	cout << "Content-type:text/html\r\n\r\n";
+	freopen("文章.txt","r",stdin);
+	cin>>ans;
+	getline(cin,name);
+	for(int i=1;i<=ans;i++){
+		getline(cin,s[i].mz);
+		getline(cin,s[i].zt);
+		cin>>s[i].flag;
+		getline(cin,s[i].nr);
+	}
+	freopen("CON","r",stdin);
+	cout<<"<html>"<<endl<<"<head>"<<endl<<"<h2>"<<endl<<name<<"的blog-基于zhiziblog</h2>"<<endl;
+	cout<<"<a href="<<ss<<">博客主页</a>"<<endl;
+	cout<<"<br>"<<endl;
+	ss="./个人简介.html";
+	cout<<"<a href="<<ss<<">个人简介</a>"<<endl;
+	cout<<"<br>"<<endl;
+	cout<<"</head>"<<endl;
+	cout<<"<body>"<<endl;
+	cout<<"<h3>"<<name<<"的文章</h3>"<<endl;
+	for(int i=1;i<=ans;i++){
+		if(s[i].flag==1){
+			cout<<"<a href="<<s[i].mz<<">"<<s[i].mz<<"</a>"<<endl;
+			cout<<"<i>"<<s[i].zt<<"</i>"<<endl;
+			cout<<"<br>"<<endl;
+		}
+	}
+	cout<<"<del>自豪的采用zhiziblog</del>"<<endl;
+	return 0;
+}
